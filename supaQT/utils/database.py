4efff38a3a3776b998c52964d1409e2735f63c75
Dan <51533129+the2dl@users.noreply.github.com>
2024-11-26 09:14:10 -0500

    Update database.py
    cleanup

--- supaQT/utils/database.py
+++ supaQT/utils/database.py
@@ -3,9 +3,9 @@ from datetime import datetime, timedelta
 import os
 
 # Supabase configuration
-SUPABASE_URL = os.getenv('SUPABASE_URL', "https://aegfzwdrslyhgsugoecw.supabase.co")
-SUPABASE_KEY = os.getenv('SUPABASE_KEY', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlZ2Z6d2Ryc2x5aGdzdWdvZWN3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNTI5NDM3MiwiZXhwIjoyMDMwODcwMzcyfQ.F_8dfPIk60brW4ZLXcBCph45KOe6jEUdSZHEikJuqhs")
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', "sk-proj-LQ5dm3iwTPD8Gkg3ys0ET3BlbkFJYrX98APv3iBjcHHRUNEU")
+SUPABASE_URL = os.getenv('SUPABASE_URL', "https://blahgoecw.supabase.co")
+SUPABASE_KEY = os.getenv('SUPABASE_KEY', "your_supa_key")
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', "your_oia_key")
 
 # Create a Supabase client instance
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
